ae961e7f272d1ae40803521329e7950d6627dbf8
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-03-05 23:56:51 +0100

    silence UnconsumedParameterWarning for rpc-retry-attempts

--- luigi/interface.py
+++ luigi/interface.py
@@ -49,7 +49,7 @@ class core(task.Config):
     This is arguably a bit of a hack.
     '''
     use_cmdline_section = False
-    _ignore_unconsumed = {'autoload_range'}
+    _ignore_unconsumed = {'autoload_range', 'rpc-retry-attempts'}
 
     local_scheduler = parameter.BoolParameter(
         default=False,
